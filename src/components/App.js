e0c85873a53048a9b325e3a1b48ad1fc61f80da3
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-15 13:12:11 -0700

    I am starting the second challenge and to prep have put the components from the first exercise into a folder and have un hooked them from App

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import Colors from './Colors';
+import React, { Component } from 'react';
 
-export default function App() {
-  return (
-    <>
-      <Colors colors={[{ name: 'Red', hex: '#FF0000', rgb: '255,0,0' }, { name: 'Green', hex: '#008000', rgb: '0,128,0' }]}/>
-    </>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        YO
+      </>
+    );
+  }
 }
+
