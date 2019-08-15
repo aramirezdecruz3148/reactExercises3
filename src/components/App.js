0289c8f2cc4a23fd82a17e2d793fd8c962e37c53
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-15 16:18:23 -0700

    I moved the last challenge into a folder and unplugged it from app

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import TextFormatter from './TextFormatter';
-
 
 export default class App extends Component {
   render() {
     return (
       <>
-        <TextFormatter />
+        YO
       </>
     );
   }
